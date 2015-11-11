cb5343be7934115f6b88972f2fb98c8070de1c33
Lauri Mäkinen <lauri.sakari.makinen@gmail.com>
2015-11-11 20:30:22 +0200

    First working version connecting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate irc;
+
+use irc::client::prelude::*;
 
 fn main() {
+	println!("Webscale scaling up...");
+
+	let server = IrcServer::new("config.json").unwrap();
+	server.identify().unwrap();
+
+	for message in server.iter() {
+		println!("got a message");
+	}
 }
