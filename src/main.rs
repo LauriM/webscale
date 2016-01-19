93dffe05c52035e542b495da2530c62c8c019f0f
David Kushner <root@davidkushner.me>
2016-01-18 20:14:00 -0800

    Implement listening, URL parsing and title feedback.

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,59 @@
 extern crate irc;
 extern crate hyper;
 extern crate xml;
+extern crate regex;
 
 use irc::client::prelude::*;
 
 use std::io::Read;
-
+use regex::Regex;
 use hyper::Client;
 use hyper::header::Connection;
 
 fn main() {
 	println!("Webscale scaling up...");
+    let config = Config {
+        nickname: Some(format!("webscale")),
+        alt_nicks: Some(vec![format!("webscale0x"), format!("webtech0")]),
+        server: Some(format!("irc.freenode.net")),
+        channels: Some(vec![format!("#0x")]),
+        .. Default::default()
+    };
 
-	let mut client = Client::new();
-
-	let server = IrcServer::new("config.json").unwrap();
+    // Setup IRC server.
+	let server = IrcServer::from_config(config).unwrap();
 	server.identify().unwrap();
 
-	for command in server.iter_cmd() {
+    // Setup HTTP client.
+    let mut client = Client::new();
 
-		if let Ok(Command::PRIVMSG(chan, msg)) = command { // Ignore errors.
-			if msg.contains("http:") {
-				for x in msg.split(' ') {
-					if(x.contains("http:"))
-					{
-						println!("Url received: {}", x);
+    // Common regex patterns.
+    let url_pattern = Regex::new(r"(http[s]?://[^\s]+)").unwrap();
+    let title_pattern = Regex::new(r"(?i)<title>(.+)</title>").unwrap();
 
-						let mut res = client.get(x).header(Connection::close()).send().unwrap();
+	for message in server.iter() {
+        let message = message.unwrap();
+        if &message.command[..] == "PRIVMSG" {
+            if let (Some(prefix), Some(msg)) = (message.prefix, message.suffix) {
+                let user = prefix.split("!")
+                    .collect::<Vec<&str>>()
+                    .first()
+                    .unwrap()
+                    .to_string();
 
-						let mut body = String::new();
-						res.read_to_string(&mut body).unwrap();
-					}
-				}
+                if url_pattern.is_match(&msg) {
+                    let url = url_pattern.captures(&msg).unwrap().at(0).unwrap();
 
-				//server.send_privmsg(&chan, "Hi!").unwrap();
-			}
-		}
-
+                    let mut res = client.get(url).send().unwrap();
+                    let mut body = String::new();
+                    res.read_to_string(&mut body).unwrap();
+                    
+                    if title_pattern.is_match(&body) {
+                        let title = title_pattern.captures(&body).unwrap().at(1).unwrap();
+                        server.send_privmsg(&message.args[0], &vec!["[", &user, "] ", title].join(""));
+                    }
+                }
+            }
+        }
 	}
 }
