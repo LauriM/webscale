d89720b0d80890d3fa43d20bf4b464853b6dafdd
Lauri Mäkinen <lauri.sakari.makinen@gmail.com>
2016-06-23 00:33:39 +0300

    WIP url fetching & title parsing

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate irc;
 extern crate xml;
 extern crate regex;
 extern crate config;
+extern crate url;
 
 use std::path::Path;
 use irc::client::prelude::*;
@@ -13,6 +14,9 @@ use hyper::header::Connection;
 use config::reader;
 use config::types::Value;
 use config::types::ScalarValue;
+use hyper::Url;
+use hyper::client::Request;
+use url::ParseError;
 
 fn main() {
 	println!("Webscale scaling up...");
@@ -48,7 +52,7 @@ fn main() {
 	server.identify().unwrap();
 
     // Setup HTTP client.
-    let mut client = Client::new();
+    let client = Client::new();
 
     // Common regex patterns.
     let url_pattern = Regex::new(r"(http[s]?://[^\s]+)").unwrap();
@@ -68,14 +72,21 @@ fn main() {
 					// Crash probably somewhere around here, not handling error correctly
                     let url = url_pattern.captures(&msg).unwrap().at(0).unwrap();
 
-                    let mut res = client.get(url).send().unwrap();
-                    let mut body = String::new();
-                    res.read_to_string(&mut body).unwrap();
-                    
-                    if title_pattern.is_match(&body) {
-                        let title = title_pattern.captures(&body).unwrap().at(1).unwrap();
-                        server.send_privmsg(&message.args[0], &vec!["Title: ", title].join(""));
-                    }
+					let mut res = client.get(url).send().unwrap();
+					assert_eq!(res.status, hyper::Ok);
+
+					// check that its ok
+					if res.status == hyper::Ok {
+
+						let mut body = String::new();
+						res.read_to_string(&mut body).unwrap();
+
+						if title_pattern.is_match(&body) {
+							let title = title_pattern.captures(&body).unwrap().at(1).unwrap();
+							server.send_privmsg(&message.args[0], &vec!["Title: ", title].join(""));
+						}
+					}
+
                 }
             }
         }
