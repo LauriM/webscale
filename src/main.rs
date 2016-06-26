a3e0b2ea0875721dc700e8cc2d953a9f444350e7
Lauri Mäkinen <lauri.sakari.makinen@gmail.com>
2016-06-26 23:10:56 +0300

    Url detection pattern reimplemented

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 extern crate hyper;
 extern crate irc;
 extern crate config;
+extern crate regex;
 
 use irc::client::prelude::*;
 use std::path::Path;
 use config::reader;
 use hyper::Client;
+use regex::Regex;
 
 fn main() {
 	println!("Webscale scaling up...");
@@ -36,6 +38,9 @@ fn main() {
     // Setup HTTP client.
     let client = Client::new();
 
+    // Used to catch the url's from incoming messages
+    let url_pattern = Regex::new(r"(http[s]?://[^\s]+)").unwrap();
+
 	for message in server.iter() {
 		let message = message.unwrap(); //If IRC message doesn't unwrap, we probably lost connection
 
@@ -44,8 +49,14 @@ fn main() {
 		match message.command {
 			Command::PRIVMSG(ref target, ref msg) => {
 
-                if(msg.contains("!ping")){
+                if(msg.contains("!ping")) {
                     server.send_privmsg(target, "pong");
+                }
+
+                if url_pattern.is_match(&msg) {
+                    let url = url_pattern.captures(&msg).unwrap().at(0).unwrap();
+
+                    println!("We should fetch url: {}", url);
                 }
 
 			},
@@ -53,4 +64,6 @@ fn main() {
 		}
 
 	}
+
+    println!("Lost connection, shutting down...");
 }
